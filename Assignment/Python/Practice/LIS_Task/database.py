636b6eeca8ecb588c336cbcb4d8414b65604feaa
paras-rana11 <parasrana11.healthray@gmail.com>
2025-07-11 18:27:41 +0530

    add

--- Assignment/Python/Practice/LIS_Task/database.py
+++ Assignment/Python/Practice/LIS_Task/database.py
@@ -2,8 +2,8 @@ import os
 
 os.chdir("C:\\practice\\Assignment\\Python\\Practice\\LIS_Task\\task1\\")
 
-au_480_path = "C:\\practice\\Assignment\\Python\\Practice\\LIS_Task\\task2\\au_480.txt"
 
+# ASTM Protocol
 def au_480(file_path):
     
     MACHINE_NAME = 'AU_480'
@@ -48,18 +48,51 @@ def au_480(file_path):
             result.append(tup)
     return result
 
-au_480_output = au_480(au_480_path)
+# au_480_path = "C:\\practice\\Assignment\\Python\\Practice\\LIS_Task\\task2\\au_480.txt"
+# au_480_output = au_480(au_480_path)
 
-print("\n\n-> FINAL au_480 OUTPUT: ")
-for op in au_480_output:
-    print(op)
+# print("\n\n-> FINAL au_480 OUTPUT: ")
+# for op in au_480_output:
+#     print(op)
     
+# HL7 Protocol
 def bc_5150(file_path):
+    MACHINE_NAME = 'BC_5150'
+    result = []
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    control_chars = ['\x02', '\x03', '\x04', '\x05', '\x06', '\x15', '\x17', '\x1c', '\x0b']
+    for sym in control_chars:
+        data = data.replace(sym, '')
+
+    # Split into messages using MSH as the start of a new message
+    data_blocks = data.strip().split('MSH')
+    print(f"\nTotal messages found: {len(data_blocks)-1}")  # First split part may be empty
+
+    for block in data_blocks:
+        block = block.strip()
+        if not block:
+            continue
+
+        lines = block.split('\n')  # In case \r replaced with \n
+        obx_data = []
+
+        for line in lines:
+            segments = line.strip().split('|')
+          
+       
+    
     
     
     
 
+bc_5150_path = "C:\\practice\\Assignment\\Python\\Practice\\LIS_Task\\task2\\bc_5150.txt"
+bc_5150_output = bc_5150(bc_5150_path)
 
-
+print("\n\n-> FINAL bc_5150 OUTPUT: ")
+for op in bc_5150_output:
+    print(op)
 
 
